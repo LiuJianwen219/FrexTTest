c9d4c0fa3dda5a26516a9d5bbfbe248e4d56826e
LiuJianwen219 <jianwen_liu219@163.com>
2021-10-12 13:50:34 +0800

    feat: test deployment

--- test.py
+++ test.py
@@ -1,57 +1,34 @@
 import json
 import logging
 import os
+import threading
+import time
+import _thread
 
 import requests
 from Constant import constants as const
 logger = logging.getLogger(__name__)
 
-def file_writer(fire_dir, file_name, file_content):
-    if not os.path.exists(fire_dir):
-        os.makedirs(fire_dir)
-    with open(os.path.join(fire_dir, file_name), 'wb+') as destination:
-        destination.write(file_content)
-    if len(file_content) < 1:
-        return None
-    return 1
+lock = threading.Lock()
 
-def get_tests():
-    userId = "1dcdba10-0d64-11ec-9d14-00d86134f158"
-    testId = "412c72d4-1e80-11ec-9233-12dcc95ac57f"
-    submitId = "2eede7bc-283d-11ec-bf42-1a069e0b09f6"
-    topic = "mod60"
-    url = const.file_server_url + const.rpts_API + "/"
-    values = {
-        const.c_userId: userId,
-        const.c_testId: testId,
-        const.c_submitId: submitId,
-        const.c_topic: topic
-    }
-    r = requests.get(url, params=values)
-    if r.status_code.__str__() != "200":
-        logger.error("Request failed: " + r.headers.__str__())
-        return const.request_failed
+t = 0
+a = [0,1,1,0,0]
 
-    print(r.content.__str__().find("Slice LUTs"))
-    index = r.content.__str__().find("Slice LUTs")
-    print(r.content.__str__()[index:r.content.__str__().find("\\n", index)].split("|"))
-    strs = r.content.__str__()[index:r.content.__str__().find("\\n", index)].split("|")
-    print(int(strs[1]))
-    
-    print(r.content.__str__().find("Slice Registers"))
-    index = r.content.__str__().find("Slice Registers")
-    print(r.content.__str__()[index:r.content.__str__().find("\\n", index)].split("|"))
-    strs = r.content.__str__()[index:r.content.__str__().find("\\n", index)].split("|")
-    print(int(strs[1]))
-    
+def printMY(str):
+    lock.acquire()
+    n = 0
+    for i in a:
+        if i:
+            n += 1
+    print("ljw: " + str(n))
 
-    if r.headers['content-type'] == "application/octet-stream" and r.content:
-        dest_direction = const.work_dir
-        dest_filename = topic + const.rpts_suffix
-        if not file_writer(dest_direction, dest_filename, r.content):
-            return const.request_failed
-        return const.request_success
-    return const.request_failed
+    # time.sleep(1)
+    lock.release()
 
 if __name__ == "__main__":
-    get_tests()
+    time_start = time.time()
+    _thread.start_new_thread(printMY, ("Thread-1",))
+    _thread.start_new_thread(printMY, ("Thread-2",))
+    time.sleep(3)
+    time_end = time.time()
+    print('time cost', time_end - time_start, 's')
